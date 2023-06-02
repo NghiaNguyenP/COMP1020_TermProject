5c118bb08a2547f4a65a80121fb3ab7183a73a88
NghiaNguyenP <132031082+NghiaNguyenP@users.noreply.github.com>
2023-06-02 21:03:15 +0700

    Update leaderboard
    need change UI

--- COMP1020_TermProject/Leaderboard.java
+++ COMP1020_TermProject/Leaderboard.java
@@ -1,16 +1,24 @@
 package COMP1020_TermProject;
 
 import java.util.*;
+import java.awt.BorderLayout;
+import java.awt.Font;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.Scanner;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import java.io.PrintWriter;
 import java.io.IOException;
 
 
 public class Leaderboard {
+    JFrame leaderboardFrame = new JFrame();
     public String name;
     private int score = 0;
     private int time;
@@ -24,40 +32,58 @@ public class Leaderboard {
             time = Integer.parseInt(sc.nextLine());
             while (sc.hasNextLine())
                 leaderboard.add(sc.nextLine());
-            addName(time, this.score);
         }
         catch(Exception e){
             System.out.print(e);
         }
+        initUI();
     }
-    public static void addName(int time, int score){
-        if (leaderboard.size() > 10){
-            leaderboard.pollFirst();
-        }
-        String st = "Attempt " + time;
-        System.out.println(String.valueOf(time).length());
-        for (int i = 0; i < 20 - String.valueOf(time).length(); i++){
-            st += " ";
-        }
-        st += score;
-        leaderboard.add(st);
-        if (time > 1000){
-            time = -1;
-        }
-        try{
-            PrintWriter out = new PrintWriter("leaderboard.txt"); 
-
-            time += 1;
-            out.println(String.valueOf(time));   
-
-            for (int i = 0; i < leaderboard.size(); i++){
-                out.println(leaderboard.get(i));
+    private void initUI() {
+        leaderboardFrame.setTitle("Leaderboard");
+        leaderboardFrame.setLayout(new BorderLayout());
+        leaderboardFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    
+        List<String> leaderboardData = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("leaderboard.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                leaderboardData.add(line);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-            out.close(); 
+        Collections.sort(leaderboardData, new Comparator<String>() {
+            @Override
+            public int compare(String entry1, String entry2) {
+                int score1 = Integer.parseInt(entry1.split(" ")[1]);
+                int score2 = Integer.parseInt(entry2.split(" ")[1]);
+                return Integer.compare(score2, score1);
+            }
+        });
+        
+        JLabel titleLabel = new JLabel("LEADERBOARD");
+        titleLabel.setFont(new Font("SansSerif", Font.BOLD, 18));
+        titleLabel.setHorizontalAlignment(JLabel.CENTER);
+        leaderboardFrame.add(titleLabel, BorderLayout.NORTH);
+    
+        JLabel scoreLabel = new JLabel("Score: " + score);
+        scoreLabel.setFont(new Font("SansSerif", Font.PLAIN, 16));
+        scoreLabel.setHorizontalAlignment(JLabel.CENTER);
+        leaderboardFrame.add(scoreLabel, BorderLayout.CENTER);
+        
+        JPanel leaderboardPanel = new JPanel();
+        leaderboardPanel.setLayout(new BoxLayout(leaderboardPanel, BoxLayout.Y_AXIS));
+
+        for (String entry : leaderboardData) {
+            JLabel entryLabel = new JLabel(entry);
+            entryLabel.setFont(new Font("SansSerif", Font.PLAIN, 16));
+            entryLabel.setHorizontalAlignment(JLabel.CENTER);
+            leaderboardPanel.add(entryLabel);
         }
-        catch(Exception e){
-            System.out.print(e);
-        }
+        leaderboardFrame.add(leaderboardPanel, BorderLayout.CENTER);
+
+        leaderboardFrame.pack();
+        leaderboardFrame.setVisible(true);
     }
 }
